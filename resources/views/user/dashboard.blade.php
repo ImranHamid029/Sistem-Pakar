855865ca604482c57bc2dc9748162329bc81fb31
ImranHamid029 <hamidimrann19@gmail.com>
2024-12-19 08:39:01 +0700

    Commit 1

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -25,19 +25,20 @@
         <div class="col-sm-6 col-md-3" data-bs-toggle="modal" data-bs-target="#change">
             <div class="card card-stats card-round">
                 <div class="card-body">
+                <a href="{{ route('diagnosa') }}" class="collapsed" aria-expanded="false">
                     <div class="row align-items-center">
                         <div class="col-icon">
                             <div class="icon-big text-center icon-danger bubble-shadow-small">
-                                <i class="fas fa-key"></i>
+                            <i class="fas fa-diagnoses"></i>
                             </div>
                         </div>
                         <div class="col col-stats ms-3 ms-sm-0">
                             <div class="numbers">
-                                <p class="card-category" data-bs-toggle="tooltip" title="tekan untuk mengganti password">Ganti Password</p>
+                                <p class="card-category" data-bs-toggle="tooltip" title="Diagnosa penyakit">Diagnosa</p>
                             </div>
                         </div>
                     </div>
-                </div>
+                </div></a>
             </div>
         </div>
     </div>
